a2988a960b9c5c23e28d355523f4521288d549c6
xingy1201 <114470026+xingy1201@users.noreply.github.com>
2022-09-27 15:16:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: 'xW_rrjZ4c-JjK_21fZwb7LVCM_xMhNRHILhrb6ug1K0',
+      useTemplateId: '	xW_rrjZ4c-JjK_21fZwb7LVCM_xMhNRHILhrb6ug1K0',
       // 所在省份
       province: '广东',
       // 所在城市
